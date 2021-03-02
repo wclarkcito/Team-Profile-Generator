53aa59eb5f36d351d8119cb23b6b4b84a9a4d2ae
warren clark <wclarkcito@gmail.com>
2021-03-02 13:59:52 -0800

    scrapped previous test code and completely re wrote everything.

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,2 +1,21 @@
-const Employee = require("./employee");
+const { it, expect, beforeEach } = require("@jest/globals");
+const { describe } = require("yargs");
+const Employee = require("../lib/employee");
+jest.mock('../lib/employee');
 
+beforeEach(() => {
+    Employee.mockClear();
+
+})
+it('user called the class constructor', () => {
+    const employee = new Employee()
+    expect(Employee).toHaveBeenCalledTimes(1)
+})
+it('user can get name of employee', async () => {
+
+    const employee = new Employee('name', 'id', 'email')
+    let spy = jest.spyOn(employee, 'getName').mockImplementation(() => 'name')
+    expect(employee.getName()).toBe('name')
+    spy.mockRestore()
+
+})
